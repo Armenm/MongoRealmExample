f75b354274b42fc37f805eb56f173960c2a885c8
Armen Mkrtchian <amkrt02@safeway.com>
2021-01-19 09:52:15 -0500

    Initial Commit

---
+++ Shared/MongoRealmExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MongoRealmExampleApp.swift
+//  Shared
+//
+//  Created by Armen Mkrtchian on 2021-01-19.
+//
+
+import SwiftUI
+
+@main
+struct MongoRealmExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
